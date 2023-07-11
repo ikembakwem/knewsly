89c6ac27fd38939f5a344198273c932dcb1f9a13
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-07-11 12:23:38 +0100

    Refactored fetcher function to use axios, implemented SSG

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,22 @@
-import { ArticlesContainer, SectionTitle } from "../components/styles";
+import {
+  ArticlesContainer,
+  SectionTitle,
+} from "../components/styles";
 import { ArticleCard } from "../components/ArticleCard";
-import { LoadingSpinner } from "../components/loaders";
-import { useState, useEffect } from "react";
 import fetcher from "../lib/fetcher";
 import { FeaturedArticleCard } from "../components/FeaturedArticleCard";
 import { Footer } from "../components/Footer";
 
-export default function Hone({ result }) {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setData(result);
-    setLoading(false);
-  }, [result]);
-
-  if (loading) {
-    return (
-      <>
-        <ArticlesContainer>
-          <SectionTitle>Sports News</SectionTitle>
-          <LoadingSpinner />
-        </ArticlesContainer>
-      </>
-    );
-  }
+export default function Hone({ posts }) {
   return (
     <>
       <ArticlesContainer>
         <SectionTitle>Sports News</SectionTitle>
       </ArticlesContainer>
       <ArticlesContainer>
-        <FeaturedArticleCard data={data[0]} />
-        {data.slice(1).map((data) => (
-          <ArticleCard key={Math.floor(Math.random() * 99999)} data={data} />
+        <FeaturedArticleCard data={posts[0]} />
+        {posts.slice(1).map((post) => (
+          <ArticleCard key={post.title} data={post} />
         ))}
       </ArticlesContainer>
       <Footer />
@@ -41,10 +24,11 @@ export default function Hone({ result }) {
   );
 }
 
-export async function getServerSideProps() {
-  const result = await fetcher("sports");
+export async function getStaticProps() {
+  const posts = await fetcher("sports");
 
   return {
-    props: { result },
+    props: { posts },
+    revalidate: 60 * 5,
   };
 }
